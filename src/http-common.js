89e8ef78e2e7fd3e350ce9181d900a78dbcd8098
Michelle Margaret <99129484+michellemargaret@users.noreply.github.com>
2022-12-01 18:01:38 -0330

    Update http-common.js
    Bug fix

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://cors-everywhere.herokuapp.com/http://grocerylistapi.ca/api",
+  baseURL: "http://grocerylistapi.ca/api",
   headers: {
     "Content-type": "application/json",
   },
