086d6607265bc4d3b5036d95b7b0829a7ed7d2ee
Michelle Margaret <99129484+michellemargaret@users.noreply.github.com>
2022-12-05 19:58:56 -0330

    Update http-common.js
    backend url update

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://grocerylistapi.ca/api",
+  baseURL: "http://mygrocerylist-env.eba-mqvynsm9.us-east-1.elasticbeanstalk.com/api",
   headers: {
     "Content-type": "application/json",
   },
